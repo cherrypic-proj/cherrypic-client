db361a7502b6fe825a8ae1c4832b23e6a1fbe943
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-07-18 01:42:31 +0900

    [#1] 폰트, 컬러, 기본 이미지 세팅

---
+++ lib/core/constants/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color mainRed = Color(0xFF9F0D17);
+  static const Color mainLightRed = Color(0xFFFACCCF);
+}
---
+++ lib/core/constants/font.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppFont {
+  static const String family = 'Koulen';
+
+  static const TextStyle size12 = TextStyle(
+    fontFamily: family,
+    fontSize: 12,
+    height: 1.4,
+  );
+
+  static const TextStyle size14 = TextStyle(
+    fontFamily: family,
+    fontSize: 14,
+    height: 1.4,
+  );
+
+  static const TextStyle size16 = TextStyle(
+    fontFamily: family,
+    fontSize: 16,
+    height: 1.5,
+  );
+
+  static const TextStyle size18 = TextStyle(
+    fontFamily: family,
+    fontSize: 18,
+    height: 1.5,
+  );
+
+  static const TextStyle size20 = TextStyle(
+    fontFamily: family,
+    fontSize: 20,
+    height: 1.5,
+  );
+}
