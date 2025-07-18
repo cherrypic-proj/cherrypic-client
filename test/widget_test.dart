77cd0d234ac0b861cb40a3548c4286514681f429
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-07-18 12:25:39 +0900

    [#1] 테스트 코드 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:cherrypic/presentation/screens/login/login_screen.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// void main() {
+//   testWidgets('로그인 화면에 텍스트가 보이는지 확인', (WidgetTester tester) async {
+//     await tester.pumpWidget(
+//       const MaterialApp(
+//         home: LoginScreen(),
+//       ),
+//     );
 
-import 'package:cherrypic/main.dart';
+//     expect(find.text('로그인 / 회원가입'), findsOneWidget);
+//   });
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+//   testWidgets('CherryPic 로고 이미지가 있는지 확인', (WidgetTester tester) async {
+//     await tester.pumpWidget(
+//       const MaterialApp(
+//         home: LoginScreen(),
+//       ),
+//     );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//     expect(find.byType(Image), findsNWidgets(4)); // logo, title, kakao/apple 아이콘
+//   });
+// }
