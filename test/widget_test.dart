8290bcf268a67164e736b50ad17836b274e14918
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-07-18 12:30:26 +0900

    [#1] 테스트 주석 해제

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,25 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:cherrypic/presentation/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:cherrypic/presentation/screens/login/login_screen.dart';
 
-// void main() {
-//   testWidgets('로그인 화면에 텍스트가 보이는지 확인', (WidgetTester tester) async {
-//     await tester.pumpWidget(
-//       const MaterialApp(
-//         home: LoginScreen(),
-//       ),
-//     );
+void main() {
+  testWidgets('로그인 화면에 텍스트가 보이는지 확인', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
 
-//     expect(find.text('로그인 / 회원가입'), findsOneWidget);
-//   });
+    expect(find.text('로그인 / 회원가입'), findsOneWidget);
+  });
 
-//   testWidgets('CherryPic 로고 이미지가 있는지 확인', (WidgetTester tester) async {
-//     await tester.pumpWidget(
-//       const MaterialApp(
-//         home: LoginScreen(),
-//       ),
-//     );
+  testWidgets('CherryPic 로고 이미지가 있는지 확인', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: LoginScreen(),
+      ),
+    );
 
-//     expect(find.byType(Image), findsNWidgets(4)); // logo, title, kakao/apple 아이콘
-//   });
-// }
+    expect(find.byType(Image), findsNWidgets(4)); // logo, title, kakao/apple 아이콘
+  });
+}
