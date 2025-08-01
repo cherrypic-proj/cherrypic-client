cdd27e89e365f53e078e4b218ea7b9f63e74d6fd
Funital <gksrmawns0925@naver.com>
2025-08-02 00:06:51 +0900

    [#2] 공통 컴포넌트 제작
    - 코드 정리
    
    #2

--- lib/app/cherrypic_app.dart
+++ lib/app/cherrypic_app.dart
@@ -1,11 +1,6 @@
-import 'package:cherrypic/presentation/screens/test/box_card_test.dart';
-import 'package:cherrypic/presentation/screens/test/button_test.dart';
 import 'package:flutter/material.dart';
 import 'package:cherrypic/presentation/screens/main/main_screen.dart';
 import 'package:cherrypic/core/constants/font.dart';
-
-import '../presentation/screens/main/main_album_screen.dart';
-import '../presentation/screens/test/main_album_test.dart';
 
 class CherrypicApp extends StatelessWidget {
   const CherrypicApp({super.key});
@@ -19,11 +14,7 @@ class CherrypicApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
-      // home: const MainScreen(),
-      // home: const ButtonTestScreen(),
-      // home: const BoxCardTest(),
-      // home: const MainAlbumTest(),
-      home: const MainAlbumScreen(),
+      home: const MainScreen(),
     );
   }
 }
--- lib/presentation/screens/test/button_test.dart
+++ lib/presentation/screens/test/button_test.dart
@@ -8,33 +8,16 @@ class ButtonTestScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('버튼 테스트'),
-        backgroundColor: Colors.red,
-      ),
+      appBar: AppBar(title: const Text('버튼 테스트'), backgroundColor: Colors.red),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             const DisabledButton(text: '앨범 생성'),
             const SizedBox(height: 16),
-            EnabledButton(
-              text: '앨범 생성',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Outlined 버튼 클릭됨')),
-                );
-              },
-            ),
+            EnabledButton(text: '앨범 생성', onPressed: () {}),
             const SizedBox(height: 16),
-            PressedButton(
-              text: '앨범 생성',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Filled 버튼 클릭됨')),
-                );
-              },
-            ),
+            PressedButton(text: '앨범 생성', onPressed: () {}),
             SquaredMenuButton(text: '공유', onPressed: () {}),
             SquaredMenuButton(text: '다운로드', onPressed: () {}),
             SquaredMenuButton(text: '삭제', onPressed: () {}),
@@ -42,25 +25,9 @@ class ButtonTestScreen extends StatelessWidget {
             RoundedMenuButton(text: '유사한 사진 그룹화', onPressed: () {}),
             RoundedMenuButton(text: '흔들린 사진 삭제', onPressed: () {}),
             RoundedMenuButton(text: '눈 감은 사진 삭제', onPressed: () {}),
-            ExitAlbumButton(
-              text: '앨범 나가기',
-              // imageAsset: 'assets/images/door_open.png',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('앨범 나가기 클릭됨')),
-                );
-              },
-            ),
+            ExitAlbumButton(text: '앨범 나가기', onPressed: () {}),
             const SizedBox(height: 8),
-            UnSubscribeButton(
-              text: '앨범 구독 해지',
-              // imageAsset: 'assets/images/trash_icon.png',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('앨범 구독 해지 클릭됨')),
-                );
-              },
-            ),
+            UnSubscribeButton(text: '앨범 구독 해지', onPressed: () {}),
           ],
         ),
       ),
