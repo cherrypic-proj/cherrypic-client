3382927eec9233924cf3ede9376f89c7c207c958
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-09-11 22:33:32 +0900

    [#27] dio_client 수정 및 로그인 dto 파일 세팅

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -7,16 +7,14 @@ class DioClient {
   final Dio dio;
 
   DioClient._internal()
-      : dio = Dio(
-          BaseOptions(
-            baseUrl: 'https://your.api.base.url',
-            connectTimeout: const Duration(seconds: 10),
-            receiveTimeout: const Duration(seconds: 10),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          ),
-        ) {
-    dio.interceptors.add(LogInterceptor(responseBody: true));
+    : dio = Dio(
+        BaseOptions(
+          baseUrl: 'https://dev-api.cherrypic.today',
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 10),
+          headers: {'Content-Type': 'application/json'},
+        ),
+      ) {
+    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
   }
 }
